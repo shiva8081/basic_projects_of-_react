ce1fb52120e1ad3076855f601a65b653cbebfae2
shiva8081 <yadavshiva8755@gmail.com>
2024-04-13 21:03:55 +0530

    start

--- src/components/Color.js
+++ src/components/Color.js
@@ -1,7 +1,10 @@
+import { getByTestId } from '@testing-library/react';
 import React from 'react'
 
 export const Color = () => {
   return (
     <div>Color</div>
+    buidgfiugufigduilfgg
+    fi;divluyfviydl;fireEvent;yield;iygfi;BiquadFilterNode;iugfoubf;iyvfi;getByTestId;f;yif;ifgi;dfgigfyilfliyguyilgygfilyg
   )
 }
