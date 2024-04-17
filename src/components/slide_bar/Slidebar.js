6ae03e540cf27c21804694aa4e35ac4761d3551f
shiva8081 <yadavshiva8755@gmail.com>
2024-04-17 23:02:49 +0530

    slide_bar_image

--- src/components/slide_bar/Slidebar.js
+++ src/components/slide_bar/Slidebar.js
@@ -41,17 +41,17 @@ export const Slidebar = ({url,limit,page}) => {
 
 
   return (
-    <>
-    {!loading?(<div className='flex relative'>
+    <div className='w-full h-full flex justify-center items-center '>
+    {!loading?(<div className='flex relative shadow-2xl'>
         
         <div onClick={()=>setcurrentslide(currentslide===0?image.length-1:currentslide-1)} className='absolute flex text-white top-[225px] left-[8px] bg-[#888600] rounded-full w-8 h-8 justify-center items-center'><button>L</button>  </div>
         <div className='flex w-[600px] h-[450px]' >
         {
-           image&&image.length?<img src={image[currentslide].download_url} alt={`Slide ${currentslide}`} />:null
+           image&&image.length?<img className='rounded-lg' src={image[currentslide].download_url} alt={`Slide ${currentslide}`} />:null
         }
         </div>
         <div onClick={()=>setcurrentslide(currentslide===image.length-1? 0 :currentslide+1)} className='absolute text-white top-[225px] left-[560px] bg-[#888600] rounded-full w-8 h-8 justify-center items-center flex'> <button className=''>R</button> </div>
     </div>):'loading...please wait'}
-    </>
+    </div>
   )
 }
